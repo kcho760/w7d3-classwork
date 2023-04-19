351587f23b4e60d4001317fc581da7b84caa626a
Merve Dogan-Espaillat <1mervedogan@gmail.com>
2023-04-19 15:07:50 -0400

    controllers rspec written

--- practice-for-ch-rails-goal-app-long-practice-main/db/schema.rb
+++ practice-for-ch-rails-goal-app-long-practice-main/db/schema.rb
@@ -10,8 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_19_180124) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_19_182140) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["username"], name: "index_users_on_username"
+  end
 
 end
--- practice-for-ch-rails-goal-app-long-practice-main/spec/requests/users_spec.rb
+++ practice-for-ch-rails-goal-app-long-practice-main/spec/requests/users_spec.rb
@@ -7,4 +7,50 @@ RSpec.describe "Users", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "GET #new" do
+      it "brings up the form to create a user" do
+      allow(subject).to receive(:logged_in?).and_return(false)
+    
+      get :new 
+      expect(response).to render_template(:new)
+      end
+  end
+
+  describe "POST #create" do
+    before :each do
+        create(:user)
+        allow(subject).to receive(:current_user).and_return(User.last)
+    end
+    let(:valid_params) {User.new(username:'merve', password: 'password')}
+    let(:invalid_params) {User.new(username:'', password: 'passrd')}
+
+    context "with valid params" do
+      it "creates the user" do
+        post :create params: valid_params
+        expect(User.last.username.to eq("merve"))
+      end
+      it "redirects to user show page" do
+        post :create params: valid_params
+        expect(response).to redirect_to(user_url(User.last.id))
+      end
+    end
+
+    context "with invalid params" do
+      before :each do
+          post :create params: valid_params
+      end
+
+      it "renders new template" do
+          expect(response).to have_http_status(422)
+          expect(response).to render_template(:new)
+      end
+
+      it "add errors to flash" do
+        expect(flash[:errors]).to be_present
+      end
+    end
+
+  end
+
 end
